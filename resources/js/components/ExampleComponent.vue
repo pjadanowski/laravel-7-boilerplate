2956b5d7737126f7c245e2266c2d5d806b563410
Paweł Jadanowski <pawel.jadanowski@lppsa.com>
2020-04-16 09:18:38 +0200

    initial

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h6>Example Component</h6>
+                {{ user }}
+                 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                user: null
+            }
+        },
+        mounted() {
+            axios.get('/api/user')
+                .then(response => {
+                    this.user = response.data
+                    console.log(response.data)
+                })
+                .catch(error => console.log(error.data))
+        }
+    }
+</script>
